a82373595f9f463cc408d71a8cad8cfe8da8e748
thomas.every <thomas.every@northumbria.ac.uk>
2018-02-15 10:04:35 +0000

    Changing and commenting of Alun's code for better understanding

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <mbed.h>
+//import the relevant libraries
 #include <C12832.h>
 
 // Using Arduino pin notation
@@ -8,14 +9,19 @@ int main()
 {
     int j=0;
     lcd.cls();
+    //places the text at a certain point on the lcd
     lcd.locate(0,0);
-    lcd.printf("mbed application shield!");
+    //text output
+    lcd.printf("The output ability is amazing");
     lcd.locate(0,10);
+    //ouputs the details of the display (14 columns and 3 rows)
+    //128x32 pixels
     lcd.printf("char %dx%d : %dx%d pixels",
                lcd.columns(), lcd.rows(),
                lcd.width(), lcd.height() );
+    //draws a small circle
     lcd.circle(100, 20, 10, 1);
-
+    //a small counting method - displays the dynamic ability of the lcd
     while(true) {
         lcd.locate(0,20);
         lcd.printf("Counting : %4d",j);
